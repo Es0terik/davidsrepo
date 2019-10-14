f6296c82b7aa4315651afc1a10fe0da4c70172af
David Mems <git@es0terik.de>
2019-10-14 20:06:55 +0200

    Initial commit

---
+++ snippets/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    private static void test (int a) {
+        System.out.println("test");
+    }
+
+    public static void main(String[] args) {
+        int b = 0;
+        test(b);
+        System.out.println("Hello, World!");
+    }
+}
